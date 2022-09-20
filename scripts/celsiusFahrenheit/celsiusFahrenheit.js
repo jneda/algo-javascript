e0bc2ed5499d062d32b00b27ea99fb00483d24d7
jdane <julien.dane@gmail.com>
2022-09-20 13:47:08 +0200

    Déclaration de la fonction de conversion des températures Media query pour vue mobile

--- scripts/celsiusFahrenheit/celsiusFahrenheit.js
+++ scripts/celsiusFahrenheit/celsiusFahrenheit.js
@@ -2,8 +2,10 @@ const celsius = document.getElementById("celsius");
 const fahrenheit = document.getElementById("fahrenheit");
 const convertBtn = document.querySelector("input[type='button']");
 
-convertBtn.addEventListener("click", function () {
+function celsiusToFahrenheit() {
   if (celsius.value !== "") {
-    fahrenheit.value = celsius.value * 9/5 + 32;
+    fahrenheit.value = celsius.value * 9 / 5 + 32;
   }
-});
+}
+
+convertBtn.addEventListener("click", celsiusToFahrenheit);
