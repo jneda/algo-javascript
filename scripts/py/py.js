c1c409694f6ddc7e693c7fc589f71a32ed7407f0
jdane <julien.dane@gmail.com>
2022-09-20 16:09:54 +0200

    On gère toutes les casses possibles de 'py' dans py.js

--- scripts/py/py.js
+++ scripts/py/py.js
@@ -5,8 +5,22 @@
 
 let chaine= prompt("Saisir une chaîne :");
 
-if (!(chaine.startsWith("Py"))){
+if (!(chaine.toLowerCase().startsWith("py"))){
   chaine = "Py" + chaine;
 }  
 
 document.write(chaine);
+
+// v1
+// 1. demander une chaine
+// 2. si chaine commence par Py, l'afficher
+// 3. sinon l'afficher en ajoutant Py devant
+/* 
+let chaine= prompt("Saisir une chaîne :");
+
+if (chaine.startsWith("Py")){
+  document.write(chaine);
+}  else {
+  document.write("Py" + chaine);
+}
+ */
