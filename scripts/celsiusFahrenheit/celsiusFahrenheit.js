06af8366a3849fd31896b9b9e11b28556fe9472b
jdane <julien.dane@gmail.com>
2022-09-20 13:43:32 +0200

    Finalisé convertisseur celsius vers fahrenheit (script JS)

--- scripts/celsiusFahrenheit/celsiusFahrenheit.js
+++ scripts/celsiusFahrenheit/celsiusFahrenheit.js
@@ -1,1 +1,9 @@
+const celsius = document.getElementById("celsius");
+const fahrenheit = document.getElementById("fahrenheit");
+const convertBtn = document.querySelector("input[type='button']");
 
+convertBtn.addEventListener("click", function () {
+  if (celsius.value !== "") {
+    fahrenheit.value = celsius.value * 9/5 + 32;
+  }
+});
