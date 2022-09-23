6f2fd2139c12566b175954838cfac4412528c3bb
jdane <julien.dane@gmail.com>
2022-09-23 09:05:15 +0200

    utilisation de fonctions dans bissextile.js

--- scripts/01_bissextile/bissextile.js
+++ scripts/01_bissextile/bissextile.js
@@ -11,23 +11,54 @@ Depuis l'ajustement du calendrier grégorien, l'année est bissextile (comportan
 Source : https://fr.wikipedia.org/wiki/Ann%C3%A9e_bissextile
 */
 
+
 // On demande l'année à l'utilisateur et on convertit en nombre dans la foulée.
 const annee = Number(prompt("Saisir une année :"));
 
 // On stocke la partie invariable du message dans une variable.
 let message = "L'année " + annee + " est ";
 
+/*
 // V5 : on fait sans les variable estMultipleDe
 
 const condition1 = annee % 4 === 0 && !annee % 100 === 0;
 const condition2 = annee % 400 === 0;
 
 const estBissextile = condition1 || condition2;
+*/
 
-if (!estBissextile) {
+// V6 : on utilise une fonction
+
+// fonction utilitaires
+
+function estMultipleDe (n, diviseur) {
+  return n % diviseur === 0;
+}
+
+function estBissextile(annee) {
+  console.log(`${annee} / 4 ? ${estMultipleDe(annee, 4)} ` +
+  `${annee} / 100 ? ${estMultipleDe(annee, 100)} ` + 
+  `${annee} / 400 ? ${estMultipleDe(annee, 400)}`);
+  return (estMultipleDe(annee, 4) && !estMultipleDe(annee, 100)) ||
+  estMultipleDe(annee, 400)
+}
+
+// fonction principale
+
+if (!estBissextile(annee)) {
   message += "non ";
 }
 message += "bissextile.";
 
 // On affiche le résultat.
 document.write("<p>" + message + "</p>");
+
+// tests
+
+function log(x) {
+  console.log(`${x} est bissextile : ${estBissextile(x)}`);
+}
+
+for (let annee = 400; annee < 432; annee += 1) {
+  log(annee);
+}
