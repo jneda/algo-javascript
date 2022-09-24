89a9e8cd207379b3afb19806b49dcea12e1d7272
jneda <julien.dane@gmail.com>
2022-09-24 08:47:01 +0200

    Implémenté conversion Fahrenheit vers Celsisus Gestion d'état basique

--- scripts/04_celsiusFahrenheit/celsiusFahrenheit.js
+++ scripts/04_celsiusFahrenheit/celsiusFahrenheit.js
@@ -1,16 +1,51 @@
-// TODO: vérifier le comportement des input type number quand on entre un nombre décimal
+// fonctions de calcul de la conversion
+
+function celsiusToFahrenheit() {
+  return (fahrenheit.value = (celsius.value * 9) / 5 + 32);
+}
+
+function fahrenheitToCelsisus() {
+  return (celsius.value = ((fahrenheit.value - 32) * 5) / 9);
+}
+
+// utilitaires
+
+function isEmptyInput(inputElt) {
+  return inputElt.value === "";
+}
+
+function setInputValue(inputElt, value) {
+  inputElt.value = value;
+  console.log(typeof value);
+}
+
+// fonction de gestion de l'état de l'application
+
+function switchAppState() {
+  if (!waiting && (isEmptyInput(fahrenheit) || isEmptyInput(celsius))) {
+    waiting = true;
+  }
+
+  if (waiting) {
+    if (!isEmptyInput(celsius)) {
+      setInputValue(fahrenheit, celsiusToFahrenheit());
+    } else if (!isEmptyInput(fahrenheit)) {
+      setInputValue(celsius, fahrenheitToCelsisus());
+    }
+
+    waiting = false;
+  }
+}
 
 // on récupère les références des éléments HTML
+
 const celsius = document.getElementById("celsius");
 const fahrenheit = document.getElementById("fahrenheit");
 const convertBtn = document.querySelector("input[type='button']");
 
-// fonction de calcul de la conversion
-function celsiusToFahrenheit() {
-  if (celsius.value !== "") {
-    fahrenheit.value = celsius.value * 9 / 5 + 32;
-  }
-}
+// variable d'état
+
+let waiting = true;
 
 // on attache l'event listener à l'élément input type button
-convertBtn.addEventListener("click", celsiusToFahrenheit);
+convertBtn.addEventListener("click", switchAppState);
